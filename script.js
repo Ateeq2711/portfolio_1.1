a53b9c17dc2e0e4aa08c97c8b8940c8f764717b6
MD ATIQULLAH KHAN <119072039+Ateeq2711@users.noreply.github.com>
2024-01-23 20:53:10 +0530

    upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    loop : true,
+    speed : 1000,
+    autoplay : {
+      delay : 2000
+    },
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },breakpoints: {
+        320: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1200: {
+          slidesPerView: 3,
+          
+        },
+      },
+    });
+
+
